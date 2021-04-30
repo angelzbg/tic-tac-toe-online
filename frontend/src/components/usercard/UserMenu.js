2bfa427403f3f4f735a16e193d629a9d1ead9910
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2021-05-01 02:17:46 +0300

    optimized user menu component

--- frontend/src/components/usercard/UserMenu.js
+++ frontend/src/components/usercard/UserMenu.js
@@ -4,24 +4,14 @@ import { useDispatch } from 'react-redux';
 import { setUserAvatar } from '../../store/actions/authActions';
 import { logOut, updateUserAvatar } from '../../utils/api';
 import AvatarsModal from '../modals/avatars';
-import { shouldUserMenuClose } from './utils';
+import { handleClickOutside } from './utils';
 
 const UserMenu = ({ setMenuOpen }) => {
   const dispatch = useDispatch();
   const userMenuRef = useRef(null);
   const [avatarModalOpen, setAvatarModalOpen] = useState(false);
 
-  useEffect(() => {
-    const handleClickOutside = ({ target }) => {
-      if (shouldUserMenuClose(avatarModalOpen, userMenuRef, target)) {
-        setMenuOpen(false);
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside, { capture: true });
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside, { capture: true });
-    };
-  }, [avatarModalOpen, setMenuOpen]);
+  useEffect(() => handleClickOutside(userMenuRef, avatarModalOpen, setMenuOpen), [avatarModalOpen, setMenuOpen]);
 
   return (
     <div className="user-menu" id="user-menu" ref={userMenuRef}>
--- frontend/src/components/usercard/utils.js
+++ frontend/src/components/usercard/utils.js
@@ -1,18 +1,26 @@
-export const shouldUserMenuClose = (avatarModalOpen, userMenuRef, target) => {
-  const { id, tagName } = target;
-  const { id: parentId } = target.parentElement;
-  const { id: parentParentId } = target.parentElement.parentElement;
+export const handleClickOutside = (userMenuRef, avatarModalOpen, setMenuOpen) => {
+  const handleClickOutside = ({ target }) => {
+    const { id, tagName } = target;
+    const { id: parentId } = target.parentElement;
+    const { id: parentParentId } = target.parentElement.parentElement;
 
-  if (
-    avatarModalOpen ||
-    userMenuRef?.current?.contains(target) ||
-    id === 'user-menu' ||
-    id === 'user-menu-toggle' ||
-    (tagName === 'svg' && parentId === 'user-menu-toggle') ||
-    (tagName === 'path' && parentParentId === 'user-menu-toggle')
-  ) {
-    return;
-  }
+    if (
+      avatarModalOpen ||
+      userMenuRef?.current?.contains(target) ||
+      id === 'user-menu' ||
+      id === 'user-menu-toggle' ||
+      (tagName === 'svg' && parentId === 'user-menu-toggle') ||
+      (tagName === 'path' && parentParentId === 'user-menu-toggle')
+    ) {
+      return;
+    }
 
-  return true;
+    setMenuOpen(false);
+  };
+
+  document.addEventListener('mousedown', handleClickOutside, { capture: true });
+
+  return () => {
+    document.removeEventListener('mousedown', handleClickOutside, { capture: true });
+  };
 };
