36306063204aa96aa40b9c5bbb9ba8941c4c5c91
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2021-05-03 03:27:06 +0300

    auth form exodia xD (#21)

--- frontend/src/pages/auth/AuthFields.js
+++ frontend/src/pages/auth/AuthFields.js
@@ -4,16 +4,7 @@ const AuthFields = ({ fields, errors, onChange }) =>
   fields.map(({ name, value, type, placeholder, errorMsg, properties }, idx) => (
     <Fragment key={name}>
       <div className={`auth-input ${errors[idx] ? 'error' : ''}`}>
-        <input
-          {...{
-            name,
-            value,
-            type,
-            placeholder,
-            onChange,
-            ...properties,
-          }}
-        />
+        <input {...{ name, value, type, placeholder, onChange, ...properties }} />
         <div className={`error-box`}>{errorMsg}</div>
       </div>
       <br />
