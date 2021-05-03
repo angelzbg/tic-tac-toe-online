e125973faaab221e39e7b6a86c570de841fcf143
VladimirAngelov <68852035+VladimirAngelov@users.noreply.github.com>
2021-05-04 00:46:34 +0300

    create login form (#23)
    Co-authored-by: Vladimir <wladimirangelow@gmail.com>

--- frontend/src/pages/auth/login/index.js
+++ frontend/src/pages/auth/login/index.js
@@ -1,5 +1,13 @@
+import AuthForm from "../AuthForm";
+import { login } from "../../../utils/api";
+import { loginInputs } from "../constants";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+
 const Login = () => {
-  return <div></div>;
+  const { isLogged } = useSelector((store) => store.auth);
+  if (isLogged) return <Redirect to="/" />;
+  return <AuthForm fields={loginInputs} onSubmit={login} />;
 };
 
 export default Login;
